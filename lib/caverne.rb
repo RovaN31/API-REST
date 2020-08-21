f8587b55727027092687f9f79a3f39276b996836
RovaN31 <67941277+RovaN31@users.noreply.github.com>
2020-08-21 23:47:30 +0300

    Update caverne.rb

--- lib/caverne.rb
+++ lib/caverne.rb
@@ -2,7 +2,7 @@ class Caverne
   def initialize
   	@cavernes = HTTParty.get('https://fr.dofus.dofapi.fr/monsters?filter[where][type]=Monstres des cavernes').parsed_response #Ceci demande toutes les monstres de type 'Monstres des cavernes'
   end
-  def caverne_json #ici on enregistre les données en fichier JSON
+  def caverne_json #ici on enregistre les données dans un fichier JSON
   	File.open("./db/monstre_caverne.json", "w") do |f|
   		f.write(@cavernes)
   	end
