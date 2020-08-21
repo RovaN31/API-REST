83d9006c1a42974a0b754e1c1dec0b959f319fb8
RovaN31 <67941277+RovaN31@users.noreply.github.com>
2020-08-21 23:44:46 +0300

    Update larve.rb

--- lib/larve.rb
+++ lib/larve.rb
@@ -1,8 +1,8 @@
 class Larve
   def initialize
-  	@larves = HTTParty.get('https://fr.dofus.dofapi.fr/monsters?filter[where][type]=Larves').parsed_response
+  	@larves = HTTParty.get('https://fr.dofus.dofapi.fr/monsters?filter[where][type]=Larves').parsed_response #Ceci demande toutes les monstres de type 'Larves'
   end
-  def larve_json
+  def larve_json #Ici on enregistre les données dans un fichier JSON
   	File.open("./db/monstre_larve.json", "w") do |f|
   		f.write(@larves)
   	end
