6c394d4fca314507f9fd7b3594a884a6e84c2717
RovaN31 <67941277+RovaN31@users.noreply.github.com>
2020-08-21 23:46:43 +0300

    Update plaine_herbeuse.rb

--- lib/plaine_herbeuse.rb
+++ lib/plaine_herbeuse.rb
@@ -1,8 +1,8 @@
 class PlaineHerbeuse
   def initialize
-  	@herbeuse = HTTParty.get('https://fr.dofus.dofapi.fr/monsters?filter[where][type]=Monstres des Plaines herbeuses').parsed_response
+  	@herbeuse = HTTParty.get('https://fr.dofus.dofapi.fr/monsters?filter[where][type]=Monstres des Plaines herbeuses').parsed_response #Ceci demande toutes les monstres de type 'Monstres des Plaines herbeuses'
   end
-  def plaine_json
+  def plaine_json #Ici on enregistre les données dans un fichier JSON
   	File.open("./db/monstre_plaine_herbeuse.json", "w") do |f|
   		f.write(@herbeuse)
   	end
